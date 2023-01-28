e8a222a4c6b7c5be5a22704e0cc3b861d025f3ef
Adam Shepard <adam.w.gangl@gmail.com>
2023-01-28 15:24:25 -0600

    Added a ternary operator

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -96,11 +96,17 @@ switch (luckyNumber) {
         console.log('Second is the best.');
         break;
     case 3:
-        console.log ('Third is the one with the polka dot dress.');
+        console.log('Third is the one with the polka dot dress.');
         break;
-        default:
-            console.log('Luck is what happens when preparation meets opportunity');
+    default:
+        console.log('Luck is what happens when preparation meets opportunity');
 }
 // 18 -- Rewrite question 13 with a `ternary` operator. You'll need to do research!
-
-
+let result;
+if (adventurous) {
+    result= 'Adventures are great!';
+}
+else {
+    result= 'How about we stay home?';
+}
+console.log(result);
